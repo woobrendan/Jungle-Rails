e062a1449840c17179bc19932f06088f8d9f6ee0
Brendan Woo <woo.brendan@gmail.com>
2022-03-09 21:25:46 +0000

    add instance variables for products and categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["NAME"], password: ENV["PASSWORD"]
   
   def show
+    @products = Product.all
+    @categories = Category.all
   end
 end
