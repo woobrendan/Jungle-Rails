62437c94b66bdafa8eb9117066711b0d892a47fd
Brendan Woo <woo.brendan@gmail.com>
2022-03-10 17:59:14 +0000

    create new view for admin categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,9 +1,10 @@
 class Admin::CategoriesController < ApplicationController
-  def show
-  end
-
   def index
     @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
   end
 
   def create
