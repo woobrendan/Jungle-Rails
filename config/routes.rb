87486d8cd1d519a6f319e038659559cc7c64de91
Brendan Woo <woo.brendan@gmail.com>
2022-03-10 23:00:37 +0000

    fix destroy route to get instead of delete method

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create]
+  get '/logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
