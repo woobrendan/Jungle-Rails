f3b5091b12d2ab18fc6ef292f984a66c0233aa4c
Brendan Woo <woo.brendan@gmail.com>
2022-03-10 22:59:27 +0000

    fix user variable to instance variable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    if user.save
-      session[:user_id] = user.id
-      redirect_to '/'
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
     else
-      redirect_to '/users/new'
+      redirect_to new_user_path
     end
   end
 
