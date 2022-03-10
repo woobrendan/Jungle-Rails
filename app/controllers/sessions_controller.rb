a4a9e80dc41d44f7f2b08aa7260892b7fb308e8e
Brendan Woo <woo.brendan@gmail.com>
2022-03-10 20:32:45 +0000

    fix login links/paths, add login conditional to nav bar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,13 +10,13 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to '/'
     else
-      redirect_to '/login'
+      redirect_to new_session_path
     end
 
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/login'
+    redirect_to new_session_path
   end
 end
